ad3e573a220a44e74c85998ffdde23c19d7c193a
ehtisham-sadiq <bsef19m521@pucit.edu.pk>
2024-06-25 15:00:16 +0500

    frontend completed and implemented using streamlit

--- frontend/main.py
+++ frontend/main.py
@@ -1,93 +1,135 @@
 import streamlit as st
 from PIL import Image
 import cv2
+import tempfile
+import time
 import numpy as np
-import requests
+from keras.preprocessing import image as keras_image # type: ignore
+from support.emotion_detection import detect_emotion_in_image, detect_emotion_in_video, load_saved_model
 
+st.set_page_config(page_title="Crowd Emotion Recognition")
 st.title("Crowd Emotion Recognition")
-st.write("""
-This project implements a system for crowd emotion recognition, aiming to automatically classify the emotions present within a crowd image or video.
-""")
 
-st.sidebar.title("Options")
-option = st.sidebar.selectbox("Choose an option", ["Upload Image", "Capture Image", "Upload Video", "Capture Video", "Display Name"])
-
-# Function to upload image
-def upload_image():
-    uploaded_file = st.file_uploader("Upload Image", type=["jpg", "png", "jpeg"])
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
-
-# Function to upload video
-def upload_video():
-    uploaded_file = st.file_uploader("Upload Video", type=["mp4"])
-    if uploaded_file is not None:
-        st.video(uploaded_file)
-
-# Function to capture image from camera
-def capture_image():
-    st.write("Please look at the camera and press the 'Capture' button to take a picture.")
-    if st.button("Capture"):
-        capture = cv2.VideoCapture(0)
-        ret, frame = capture.read()
-        if ret:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            st.image(frame, caption="Captured Image", use_column_width=True)
-            capture.release()
-        else:
-            st.error("Failed to capture image.")
-
-# Function to capture video from camera
-def capture_video():
-    st.write("Please look at the camera and press the 'Start Recording' button to record a video.")
-    if st.button("Start Recording"):
-        capture = cv2.VideoCapture(0)
-        out = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 20.0, (640, 480))
-        recording = True
-        while recording:
-            ret, frame = capture.read()
-            if ret:
-                out.write(frame)
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                st.image(frame, caption="Recording...", use_column_width=True)
-                if st.button("Stop Recording"):
-                    recording = False
-            else:
-                st.error("Failed to record video.")
-                recording = False
-        out.release()
-        capture.release()
-
-def display_name():
-    name = st.text_input("Enter your name")
-    if name:
-        res = requests.post(url = "http://127.0.0.1:8000/name", data = {"name": name})
-        text = res.json()["message"]
-        text = f"Hello {text}"
-        st.write(text)
-        # st.write(res.text)
-
-if option == "Upload Image":
-    upload_image()
-elif option == "Upload Video":
-    upload_video()
-elif option == "Capture Image":
-    capture_image()
-elif option == "Capture Video":
-    capture_video()
-elif option == "Display Name":
-    display_name()
-    
-
-
-# Footer instructions
 st.markdown("""
-<footer style='text-align:center; position: fixed; bottom: 0; width: 50%;'>
-    <p><b>The future of emotional intelligence | Crowd Emotion Recognizer | Developed by Zunaira and Darban |</b></p>
-    <p><b>Supervised by Dr. Ali Raza!</b></p>
-</footer>
+    <p>This project implements a system for crowd emotion recognition, aiming to automatically classify the emotions present within a crowd image or video</p>
 """, unsafe_allow_html=True)
 
+model = load_saved_model()
 
-# streamlit run frontend/main.py
+def add_boundary():
+    st.markdown(
+        "<br><hr style='border: 1px solid white; border-radius: 2px;'>",
+        unsafe_allow_html=True
+    )
+
+def clear_cache():
+    st.session_state.clear()
+
+st.markdown("<br>", unsafe_allow_html=True)
+st.markdown("##### Browse Image or Video from the System")
+uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png", "mp4", "avi", "mov"])
+
+if uploaded_file is not None:
+    clear_cache()
+    file_type = uploaded_file.type.split('/')[0]
+    if file_type == 'image':
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image', use_column_width=False)
+        image_path = tempfile.NamedTemporaryFile(delete=False, suffix=".jpg").name
+        image.save(image_path)
+    elif file_type == 'video':
+        tfile = tempfile.NamedTemporaryFile(delete=False)
+        tfile.write(uploaded_file.read())
+        video_path = tfile.name
+        st.video(video_path)
+
+add_boundary()
+
+# Section for capturing or recording image and video
+st.markdown("##### Capture or Record Image and Video")
+capture_col1, capture_col2 = st.columns(2)
+
+captured_image = None
+captured_video_path = None
+
+with capture_col1:
+    if st.button("Capture Image"):
+        time.sleep(1)  # 1-second pause before starting the capture
+        cap = cv2.VideoCapture(0)
+        ret, frame = cap.read()
+        cap.release()
+        if ret:
+            captured_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            st.session_state['captured_image'] = captured_image
+            st.write("Image captured successfully")
+        else:
+            st.error("Failed to capture image")
+
+    if st.button("Record Video"):
+        time.sleep(1)  # 1-second pause before starting the recording
+        st.write("Recording for 5 seconds...")
+        cap = cv2.VideoCapture(0)
+        codec = cv2.VideoWriter_fourcc(*"mp4v")  # Save as MP4 format
+        video_filename = "captured_video.mp4"
+        out = cv2.VideoWriter(video_filename, codec, 20.0, (640, 480))
+        for _ in range(100): 
+            ret, frame = cap.read()
+            if ret:
+                out.write(frame)
+            else:
+                break
+        cap.release()
+        out.release()
+        captured_video_path = video_filename
+        st.session_state['captured_video_path'] = captured_video_path
+        st.write("Video recorded successfully")
+
+with capture_col2:
+    if 'captured_image' in st.session_state:
+        st.image(st.session_state['captured_image'], caption='Captured Image', use_column_width=True)
+    if 'captured_video_path' in st.session_state:
+        st.video(st.session_state['captured_video_path'])
+
+add_boundary()
+st.markdown("##### Display Result")
+result_col1, result_col2 = st.columns(2)
+
+with result_col1:
+    if st.button("Get Result"):
+        if uploaded_file is not None:
+            if file_type == 'image':
+                emotion = detect_emotion_in_image(model, image_path)
+                st.session_state['result'] = emotion
+            elif file_type == 'video':
+                emotion = detect_emotion_in_video(model, video_path)
+                st.session_state['result'] = emotion
+        elif 'captured_image' in st.session_state:
+            captured_image_path = tempfile.NamedTemporaryFile(delete=False, suffix=".jpg").name
+            st.session_state['captured_image'].save(captured_image_path)
+            emotion = detect_emotion_in_image(model, captured_image_path)
+            st.session_state['result'] = emotion
+        elif 'captured_video_path' in st.session_state:
+            emotion = detect_emotion_in_video(model, st.session_state['captured_video_path'])
+            st.session_state['result'] = emotion
+
+with result_col2:
+    if 'result' in st.session_state:
+        st.write(f"Detected Emotion: {st.session_state['result']}")
+    else:
+        st.write("No emotion detected yet.")
+
+st.markdown("""
+    <style>
+    .footer {
+        position: relative;
+        bottom: 0;
+        width: 100%;
+        text-align: center;
+    }
+    </style>
+    <div class="footer">
+        <p><b>The future of emotional intelligence | Crowd Emotion Recognizer | Developed by Zunaira and Darban |</b></p>
+        <p><b>Supervised by Dr. Ali Raza!</b></p>
+    </div>
+""", unsafe_allow_html=True)
+
---
+++ frontend/support/emotion_detection.py
@@ -0,0 +1,194 @@
+import cv2
+import numpy as np
+import seaborn as sns
+import streamlit as st
+import matplotlib.pyplot as plt
+from keras.models import load_model  # type: ignore
+from keras.preprocessing import image # type: ignore
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
+
+model_path = "model/resnet50_tt_fermod_68.h5"
+
+def load_saved_model():
+    resnet50_model = load_model(model_path)
+    return resnet50_model
+
+def emotion_analysis(emotions):
+    objects = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+    y_pos = np.arange(len(objects))
+    
+    sns.set(style="whitegrid")
+    sns.barplot(x=objects, y=emotions, palette="viridis")
+    plt.ylabel('Percentage')
+    plt.title('Emotion Analysis')
+    plt.show()
+
+# def emotion_analysis(emotions):
+#     objects = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+#     y_pos = np.arange(len(objects))
+    
+#     sns.set(style="whitegrid")
+#     fig, ax = plt.subplots()
+#     sns.barplot(x=objects, y=emotions, palette="viridis", ax=ax)
+#     ax.set_ylabel('Percentage')
+#     ax.set_title('Emotion Analysis')
+    
+#     st.pyplot(fig)
+
+# Function to process image and detect emotion 
+# def detect_emotion_in_image(model, image):
+#     d = {0:"angry",1:"disgust",2:"fear",3:"happiness",4:"sad",5:"surprise",6:"neutral"}
+#     face_cascade = cv2.CascadeClassifier('../opencv/haarcascade_frontalface_alt.xml')
+#     # true_image = image.load_img(image) # actual image
+#     t_image = cv2.imread(image)
+#     gray = cv2.cvtColor(t_image,cv2.COLOR_BGR2GRAY)
+#     faces = face_cascade.detectMultiScale(gray,1.1,4)
+#     if(len(faces) == 0):
+#         exit()
+#     for (x,y,w,h) in faces:
+#         cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
+#         start_row,end_row,start_col,end_col = y,y+h,x,x+h
+#     croppedimage = gray[start_row:end_row,start_col:end_col]
+#     img = cv2.resize(croppedimage,(48,48))
+#     x = image.img_to_array(img)
+#     x = np.expand_dims(x, axis = 0)
+#     x /= 255
+#     custom = model.predict(x)
+#     emotion_analysis(custom[0])
+#     print(custom[0])
+#     emt = list(custom[0])
+#     idx = emt.index(max(emt))
+#     emotion = d[idx]
+#     print(f"Emotion in the image is : {emotion}")
+#     return emotion
+
+import cv2
+import numpy as np
+from tensorflow.keras.preprocessing import image as keras_image # type: ignore
+
+def detect_emotion_in_image(model, image_path):
+    d = {0:"angry",1:"disgust",2:"fear",3:"happiness",4:"sad",5:"surprise",6:"neutral"}
+    face_cascade = cv2.CascadeClassifier('opencv/haarcascade_frontalface_alt.xml')
+    t_image = cv2.imread(image_path)
+    gray = cv2.cvtColor(t_image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    if len(faces) == 0:
+        return "No faces detected"
+    for (x, y, w, h) in faces:
+        cv2.rectangle(gray, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        start_row, end_row, start_col, end_col = y, y+h, x, x+w
+    cropped_image = gray[start_row:end_row, start_col:end_col]
+    img = cv2.resize(cropped_image, (48, 48))
+    x = keras_image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x /= 255
+    custom = model.predict(x)
+    emotion_analysis(custom[0])
+    print(custom[0])
+    emt = list(custom[0])
+    idx = emt.index(max(emt))
+    emotion = d[idx]
+    print(f"Emotion in the image is : {emotion}")
+    return emotion
+
+
+
+def getFrame(seconds, vidcap, model, face_cascade, d, dcount):
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,seconds*1000)
+    hasFrames,img = vidcap.read()
+    
+    if hasFrames:
+        minutes = "00"
+        hours = "00"
+        if seconds >= 60:
+            minutes = str(seconds//60)
+            seconds = seconds % 60
+
+        if int(minutes) >= 60:
+            hours = str(int(minutes)//60)
+            minutes = str(int(minutes) % 60)
+
+        min = "{:02d}".format(int(minutes))
+        sec = "{:02d}".format(seconds)
+        hrs = "{:02d}".format(int(hours))
+
+        flag = 0 
+        frameId = vidcap.get(1)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray,1.1,4)
+        
+        if(len(faces)==0):
+            flag = 1
+        
+        if flag == 0 :
+            global count
+            count = count + 1
+            for (x,y,w,h) in faces:
+                cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
+                start_row,end_row,start_col,end_col = y,y+h,x,x+h  
+            
+            croppedimage = gray[start_row:end_row,start_col:end_col]
+            finalimg = cv2.resize(croppedimage,(48,48))
+
+            x = image.img_to_array(finalimg)
+            x = np.expand_dims(x, axis = 0)
+
+            x /= 255
+
+            custom = model.predict(x)
+            emt = list(custom[0])
+            idx = emt.index(max(emt))
+            imgname = d[idx]
+            dcount[imgname] = dcount[imgname] + 1
+            
+            cv2.imwrite("/data/" + "%d_" % count +imgname+"__"+ hrs+":"+min+":"+sec+".jpg" ,img) 
+    
+    return hasFrames
+
+
+
+
+
+# Function to process video and detect emotion 
+def detect_emotion_in_video(model, video_path):
+    # videoFile = "/Users/ehtishamsadiq/Crowd-Emotion-Recognition/backend/data/emotion_recognition_test_video.mp4"
+    vidcap = cv2.VideoCapture(video_path)
+    d = {
+        0:"angry",
+        1:"disgust",
+        2:"fear",
+        3:"happiness",
+        4:"sad",
+        5:"surprise",
+        6:"neutral"
+        }
+    dcount = {
+        "angry":0,
+        "disgust":0,
+        "fear":0,
+        "happiness":0,
+        "sad":0,
+        "surprise":0,
+        "neutral":0
+        }
+
+    face_cascade = cv2.CascadeClassifier('opencv/haarcascade_frontalface_alt.xml')
+    count = 0
+    sec = 0
+    frameRate = 3 #it will capture image in each 2 second
+    success = getFrame(sec, vidcap, model, face_cascade, d, dcount)
+    while success:
+        sec = sec + frameRate
+        #sec = round(sec, 2)
+        success = getFrame(sec, vidcap, model, face_cascade, d, dcount)
+
+    print ("Done!")
+    print("Extracted images:",count)
+    print(dcount)
+    emotions = list(dcount.keys())
+    values = list(dcount.values())
+    for key, value in dcount.items():
+        if value == max(values):
+            emotion = key
+    emotion_analysis(values)
+    return emotion
