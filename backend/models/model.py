9825de7506dd10c67c9a8284ea707fa170f06bdb
ehtisham-sadiq <bsef19m521@pucit.edu.pk>
2024-06-25 14:58:48 +0500

    backend completed and added

--- backend/models/model.py
+++
@@ -1,3 +0,0 @@
-# defining a custom emotion recognition model architecture in PyTorch, this file would contain the code for:
-# Defining the model class with convolutional and pooling layers.
-# Implementing the forward pass for processing input data.
